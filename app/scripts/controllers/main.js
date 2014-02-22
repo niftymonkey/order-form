e5040fcf4fb1fef6e035522f25fff6b31cd00217
Mark Lozano <mark.d.lozano@gmail.com>
2014-02-22 00:19:01 -0700

    Initial commit of application foundation

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('orderFormApp')
+    .controller('MainCtrl', function ($scope, $http) {
+        $http.get('/api/awesomeThings').success(function (awesomeThings) {
+            $scope.awesomeThings = awesomeThings;
+        });
+    });
